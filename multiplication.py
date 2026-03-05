f4fb21b246dee3165a7f62b7cb7198a6c89bde8b
Saba Noureen <sabanoureen123@gmail.com>
2026-03-05 12:04:00 +0300

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,9 @@
+# Simple Multiplication Program
+# Created as part of my Python learning journey
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+result = a * b
+
+print("Multiplication result is:", result)
